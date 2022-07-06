29d3d8147befca514769c2ab27c1a1f79a0a361e
VOOSsebastien <sebastien.voos@outlook.com>
2022-07-06 12:50:32 +0200

    +css

--- src/App.js
+++ src/App.js
@@ -1,58 +1,72 @@
-import React, { useState ,useRef,useEffect} from "react";
+import React, { useState ,useRef,useEffect} from 'react';
 import TodoList from "./TodoList";
-//import uuidv4 from 'uuid/v4'
+import {v4 as uuid} from 'uuid';
+import './style.css';
 
 
 const LOCAL_STORAGE_KEY='todoApp.todos'
 
 function App() {
-  const [todos,setTodos]=useState([])
-  const todoNameRef=useRef()
+  const [todos,setTodos]=useState([]);
+  const todoNameRef=useRef();
 
 
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(todos))
-  }, [todos])
-  
   useEffect(()=>{
-    const storedTodos=JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    console.log({LOCAL_STORAGE_KEY});
-    console.log(storedTodos);
-    setTodos(storedTodos);
-    console.log(storedTodos)
-  },[])
+    const storedTodos=JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(storedTodos) setTodos(storedTodos);
+  },[]);
+  
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(todos));
+  }, [todos]);
+  
   
 
   function toggleTodo(id){
-    const newTodos=[...todos]
-    const todo=newTodos.find(todo=>todo.id===id)
-    todo.complete=!todo.complete
-    setTodos(newTodos)
+    const newTodos=[...todos];
+    const todo=newTodos.find(todo=>todo.id===id);
+    todo.complete=!todo.complete;
+    setTodos(newTodos);
   }
 
 function handleAddTodo(e){
-  const name=todoNameRef.current.value
+  const name=todoNameRef.current.value;
   if(name==='')return
   setTodos(prevTodos=>{
-    return [...prevTodos,{id:Math.random(1000000000),name:name,complete:false}]
+    return [...prevTodos,{id:uuid(),name:name,complete:false}]
   })
-  todoNameRef.current.value=null
+  todoNameRef.current.value=null;
 }
 
 
-function handleClearTodo(){
-  const newTodos=todos.filter(todo=>!todo.complete)
-  setTodos(newTodos)
+function handleClearTodo(e){
+  const newTodos=todos.filter(todo=>!todo.complete);
+  setTodos(newTodos);
 }
 
+
+/*
+if((todos.filter(todo=>!todo.complete).length)<3){
+  var x=document.querySelector('p');
+  x.setAttribute("class","toomuch");
+}else{
+  document.querySelector('p').setAttribute("class","relax");
+}
+*/
   return (
     <>
-    <TodoList todos={todos} toggleTodo={toggleTodo}/>
+    <div class="html">
+    <div class="carte">
+    <h1>TodoList</h1>
     <input ref={todoNameRef} type="text"/>
-    <button onClick={handleAddTodo}>Add Todo</button>
+    <div class="buttons">
+    <button onClick={handleAddTodo}>Add Todo </button>
     <button onClick={handleClearTodo}>Clear completed</button>
-    <div>{todos.filter(todo=>!todo.complete).length}</div>
+    </div>
+    <p id="nrTodo">{todos.filter(todo=>!todo.complete).length} tasks to do </p>
+      </div>
+  <div id="list"><TodoList todos={todos} toggleTodo={toggleTodo}/></div>
+    </div>
     </>
   );
 }
